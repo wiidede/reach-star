b0bb6845431d4a8740d6c47bae6ea9e9fe013832
wiidede <wiixdede@gmail.com>
2023-11-26 14:09:02 +0800

    feat: public

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Reach Star'
+export const appDescription = 'Reach Star✨ A goal manage application.'
